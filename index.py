4c8952fba2e7464729a11f967d8185c90f3c35bc
Dmitry <79690865+bymagadan@users.noreply.github.com>
2023-11-08 23:42:13 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+from prettytable import  PrettyTable
+
+class Students:
+    newtable = PrettyTable(["ID", "FIO", "VARIANT", "GROUP"])
+    f = open("list.txt", encoding='utf-8')
+    for i in f:
+        g = i.split(";")
+        newtable.add_row([int(g[0]), g[1], int(g[2]), int(g[3])])
+    print(newtable)
+
+
+Students()
